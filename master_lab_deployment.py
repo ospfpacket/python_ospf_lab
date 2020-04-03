06edc3fab8f855e0c5ee19674bc4d0e9422484dd
Daniel Barron <ospfpacket@gmail.com>
2020-04-03 15:45:11 -0400

    script and inventory folders created

--- master_lab_deployment.py
+++ master_lab_deployment.py
@@ -1,2 +1,10 @@
 #!/usr/bin/env python3
+import sys
+sys.path.append('/home/ospfpacket/code/python/ospf_automation_lab/scripts')
+import global_config
+import int_seg_config
+import ospf_config
 
+exec('global_config')
+exec('int_seg_config')
+exec('ospf_config')
--- scripts/global_config.py
+++ scripts/global_config.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
 import sys
-print(sys.path)
-#from netmiko import ConnectHandler
-#from inventory import routers, tunnelrouters
+sys.path.append('./inventory')
+from netmiko import ConnectHandler
+from rtr_inv import R1
 
 #Here is the config that is standard among all routers.
-#net_connect = ConnectHandler(**routers)
-#config_commands = ['ip domain name ospfpacket']
+net_connect = ConnectHandler(**routers)
+config_commands = ['ip domain name ospfpacket']
 
 #Here is the config that is standard among the tunnel routers.
-
-
