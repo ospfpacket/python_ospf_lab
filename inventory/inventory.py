ad166bd655608ef0c8ca542aa74ccc55a879b7c8
Daniel Barron <ospfpacket@gmail.com>
2020-04-03 12:35:03 -0400

    Inital commit for python lab

---
+++ inventory/inventory.py
@@ -0,0 +1,72 @@
+R1 = {
+    'device_type': 'cisco_ios',
+    'host': '10.0.0.21',
+    'username': 'daniel',
+    'password': 'z00k33p3r',
+}
+
+R2 = {
+    'device_type': 'cisco_ios',
+    'host': '10.0.0.22',
+    'username': 'daniel',
+    'password': 'z00k33p3r',
+}
+
+R3 = {
+    'device_type': 'cisco_ios',
+    'host': '10.0.0.23',
+    'username': 'daniel',
+    'password': 'z00k33p3r',
+}
+
+R4 = {
+    'device_type': 'cisco_ios',
+    'host': '10.0.0.24',
+    'username': 'daniel',
+    'password': 'z00k33p3r',
+}
+
+R5 = {
+    'device_type': 'cisco_ios',
+    'host': '10.0.0.25',
+    'username': 'daniel',
+    'password': 'z00k33p3r',
+}
+
+R6 = {
+    'device_type': 'cisco_ios',
+    'host': '10.0.0.26',
+    'username': 'daniel',
+    'password': 'z00k33p3r',
+}
+
+R7 = {
+    'device_type': 'cisco_ios',
+    'host': '10.0.0.27',
+    'username': 'daniel',
+    'password': 'z00k33p3r',
+}
+
+R8 = {
+    'device_type': 'cisco_ios',
+    'host': '10.0.0.28',
+    'username': 'daniel',
+    'password': 'z00k33p3r',
+}
+
+R9 = {
+    'device_type': 'cisco_ios',
+    'host': '10.0.0.29',
+    'username': 'daniel',
+    'password': 'z00k33p3r',
+}
+
+R10 = {
+    'device_type': 'cisco_ios',
+    'host': '10.0.0.30',
+    'username': 'daniel',
+    'password': 'z00k33p3r',
+}
+
+routers = [R1, R2, R3, R4, R5, R6, R7, R8, R9, R10]
+tunnelrouters = [R1, R2, R3, R4, R5]
---
+++ scripts/global_config.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import sys
+print(sys.path)
+#from netmiko import ConnectHandler
+#from inventory import routers, tunnelrouters
+
+#Here is the config that is standard among all routers.
+#net_connect = ConnectHandler(**routers)
+#config_commands = ['ip domain name ospfpacket']
+
+#Here is the config that is standard among the tunnel routers.
+
+
