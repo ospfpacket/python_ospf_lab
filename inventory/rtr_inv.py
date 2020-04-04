97438f9216528e1247c7123d14162a25116e3065
Daniel Barron <daniel@ospfpacket.com>
2020-04-04 08:02:04 -0400

    updating int_seg_config with remainint subnets

--- inventory/rtr_inv.py
+++ inventory/rtr_inv.py
@@ -1,86 +1,88 @@
 R1 = {
     'device_type': 'cisco_ios',
     'host': '10.0.0.21',
-    'username': 'daniel',
-    'password': 'z00k33p3r',
-    'secret': 'z00k33p3r'
+    'username': 'user1',
+    'password': 'password1',
+    'secret': 'secret1'
 }
 
 R2 = {
     'device_type': 'cisco_ios',
     'host': '10.0.0.22',
-    'username': 'daniel',
-    'password': 'z00k33p3r',
-    'secret': 'z00k33p3r'
+    'username': 'user1',
+    'password': 'password1',
+    'secret': 'secret1'
 }
 
 R3 = {
     'device_type': 'cisco_ios',
     'host': '10.0.0.23',
-    'username': 'daniel',
-    'password': 'z00k33p3r',
-    'secret': 'z00k33p3r'
+    'username': 'user1',
+    'password': 'password1',
+    'secret': 'secret1'
 }
 
 R4 = {
     'device_type': 'cisco_ios',
     'host': '10.0.0.24',
-    'username': 'daniel',
-    'password': 'z00k33p3r',
-    'secret': 'z00k33p3r'
+    'username': 'user1',
+    'password': 'password1',
+    'secret': 'secret1'
 }
 
 R5 = {
     'device_type': 'cisco_ios',
     'host': '10.0.0.25',
-    'username': 'daniel',
-    'password': 'z00k33p3r',
-    'secret': 'z00k33p3r'
+    'username': 'user1',
+    'password': 'password1',
+    'secret': 'secret1'
 }
 
 R6 = {
     'device_type': 'cisco_ios',
     'host': '10.0.0.26',
-    'username': 'daniel',
-    'password': 'z00k33p3r',
-    'secret': 'z00k33p3r'
+    'username': 'user1',
+    'password': 'password1',
+    'secret': 'secret1'
 }
 
 R7 = {
     'device_type': 'cisco_ios',
     'host': '10.0.0.27',
-    'username': 'daniel',
-    'password': 'z00k33p3r',
-    'secret': 'z00k33p3r'
+    'username': 'user1',
+    'password': 'password1',
+    'secret': 'secret1'
 }
 
 R8 = {
     'device_type': 'cisco_ios',
     'host': '10.0.0.28',
-    'username': 'daniel',
-    'password': 'z00k33p3r',
-    'secret': 'z00k33p3r'
+    'username': 'user1',
+    'password': 'password1',
+    'secret': 'secret1'
 }
 
 R9 = {
     'device_type': 'cisco_ios',
     'host': '10.0.0.29',
-    'username': 'daniel',
-    'password': 'z00k33p3r',
-    'secret': 'z00k33p3r'
+    'username': 'user1',
+    'password': 'password1',
+    'secret': 'secret1'
 }
 
 R10 = {
     'device_type': 'cisco_ios',
     'host': '10.0.0.30',
-    'username': 'daniel',
-    'password': 'z00k33p3r',
-    'secret': 'z00k33p3r'
+    'username': 'user1',
+    'password': 'password1',
+    'secret': 'secret1'
 }
 
 routers = [R1, R2, R3, R4, R5, R6, R7, R8, R9, R10]
 tunnelrouters = [R1, R2, R3, R4, R5]
 tunneloct = ['1', '2', '3', '4', '5']
+tunnelspokes = [R1, R2, R3, R4]
+tunnelspokeoct = ['1', '2', '3', '4']
 loopbacks = ['1.1.1.1', '2.2.2.2', '3.3.3.3', '4.4.4.4', '5.5.5.5', '6.6.6.6', '7.7.7.7', '8.8.8.8', '9.9.9.9', '10.10.10.10']
 hostnames = ['R1', 'R2', 'R3', 'R4', 'R5', 'R6', 'R7', 'R8', 'R9', 'R10']
 
--- scripts/int_seg_config.py
+++ scripts/int_seg_config.py
@@ -4,7 +4,7 @@ sys.path.append('./inventory')
 from netmiko import ConnectHandler
 from rtr_inv import *
 
-#configure single segment routers
+#configure single segment routers on singlesegments
 for devicelist, octets, lastoct in zip(singleseg, singlesegoct, singleseglastoct) :
     net_connect = ConnectHandler(**devicelist)
     net_connect.enable()
@@ -16,7 +16,7 @@ for devicelist, octets, lastoct in zip(singleseg, singlesegoct, singleseglastoct
     net_connect.disconnect()
 
 
-#configure interfaces and assign IP addresses
+#configure interfaces and assign IP addresses on segment 146
 for devicelist, lastoctet in zip(seg146, octet146) :
     net_connect = ConnectHandler(**devicelist)
     net_connect.enable()
@@ -27,7 +27,7 @@ for devicelist, lastoctet in zip(seg146, octet146) :
     print(output)
     net_connect.disconnect()
 
-#configure interfaces and assign IP addresses
+#configure interfaces and assign IP addresses on segment 79
 for devicelist, lastoctet in zip(seg79, octet79) :
     net_connect = ConnectHandler(**devicelist)
     net_connect.enable()
@@ -37,3 +37,134 @@ for devicelist, lastoctet in zip(seg79, octet79) :
     output = net_connect.send_config_set(config_commands)
     print(output)
     net_connect.disconnect()
+
+#configure interfaces and assign IP addresses on segment 37
+for devicelist, lastoctet in zip(seg37, octet37) :
+    net_connect = ConnectHandler(**devicelist)
+    net_connect.enable()
+    config_commands = ['interface ethernet0/0.37',
+                       'encap dot 37',
+                       'ip address 155.1.37.' + lastoctet + ' 255.255.255.0']
+    output = net_connect.send_config_set(config_commands)
+    print(output)
+    net_connect.disconnect()
+
+#configure interfaces and assign IP addresses on segment 67
+for devicelist, lastoctet in zip(seg67, octet67) :
+    net_connect = ConnectHandler(**devicelist)
+    net_connect.enable()
+    config_commands = ['interface ethernet0/0.67',
+                       'encap dot 67',
+                       'ip address 155.1.67.' + lastoctet + ' 255.255.255.0']
+    output = net_connect.send_config_set(config_commands)
+    print(output)
+    net_connect.disconnect()
+
+#configure interfaces and assign IP addresses on segment 100
+for devicelist, lastoctet in zip(tunnelrouters, tunneloct) :
+    net_connect = ConnectHandler(**devicelist)
+    net_connect.enable()
+    config_commands = ['interface ethernet0/0.100',
+                       'encap dot 100',
+                       'ip address 169.254.100.' + lastoctet + ' 255.255.255.0']
+    output = net_connect.send_config_set(config_commands)
+    print(output)
+    net_connect.disconnect()
+
+#configure interfaces and assign IP addresses on segment 13
+for devicelist, lastoctet in zip(seg13, octet13) :
+    net_connect = ConnectHandler(**devicelist)
+    net_connect.enable()
+    config_commands = ['interface ethernet0/0.13',
+                       'encap dot 13',
+                       'ip address 155.1.13.' + lastoctet + ' 255.255.255.0']
+    output = net_connect.send_config_set(config_commands)
+    print(output)
+    net_connect.disconnect()
+
+#configure interfaces and assign IP addresses on segment 23
+for devicelist, lastoctet in zip(seg23, octet23) :
+    net_connect = ConnectHandler(**devicelist)
+    net_connect.enable()
+    config_commands = ['interface ethernet0/0.23',
+                       'encap dot 23',
+                       'ip address 155.1.23.' + lastoctet + ' 255.255.255.0']
+    output = net_connect.send_config_set(config_commands)
+    print(output)
+    net_connect.disconnect()
+
+#configure interfaces and assign IP addresses on segment 45
+for devicelist, lastoctet in zip(seg45, octet45) :
+    net_connect = ConnectHandler(**devicelist)
+    net_connect.enable()
+    config_commands = ['interface ethernet0/0.45',
+                       'encap dot 45',
+                       'ip address 155.1.45.' + lastoctet + ' 255.255.255.0']
+    output = net_connect.send_config_set(config_commands)
+    print(output)
+    net_connect.disconnect()
+
+#configure interfaces and assign IP addresses on segment 58
+for devicelist, lastoctet in zip(seg58, octet58) :
+    net_connect = ConnectHandler(**devicelist)
+    net_connect.enable()
+    config_commands = ['interface ethernet0/0.58',
+                       'encap dot 58',
+                       'ip address 155.1.58.' + lastoctet + ' 255.255.255.0']
+    output = net_connect.send_config_set(config_commands)
+    print(output)
+    net_connect.disconnect()
+
+#configure interfaces and assign IP addresses on segment 108
+for devicelist, lastoctet in zip(seg108, octet108) :
+    net_connect = ConnectHandler(**devicelist)
+    net_connect.enable()
+    config_commands = ['interface ethernet0/0.108',
+                       'encap dot 108',
+                       'ip address 155.1.108.' + lastoctet + ' 255.255.255.0']
+    output = net_connect.send_config_set(config_commands)
+    print(output)
+    net_connect.disconnect()
+
+#configuretunnel interface on spoke routers
+for devicelist, lastoctet in zip(tunnelspokes, tunnelspokeoct) :
+    net_connect = ConnectHandler(**devicelist)
+    net_connect.enable()
+    config_commands = ['interface Tunnel0',
+                       'ip address 155.1.0.' + lastoctet + ' 255.255.255.0',
+                       'ip mut 1400',
+                       'ip nhrp authentication NHRPPASS',
+                       'ip nhrp map 155.1.0.5 169.254.100.5',
+                       'ip nhrp map multicast 169.254.100.5'
+                       'ip nhrp network-id 1',
+                       'ip nhrp holdtime 300',
+                       'ip nhrp nhs 155.1.0.5',
+                       'ip tcp adjust-mss 1360',
+                       'tunnel source Ethernet0/0.100',
+                       'tunnel mode gre multipoint',
+                       'tunnel key 150',
+                       'tunnel protection ipsec profile DMVPN_PROFILE',
+                       'no shutdown']
+    output = net_connect.send_config_set(config_commands)
+    print(output)
+    net_connect.disconnect()
+
+#configure tunnel interface on hub router
+net_connect = ConnectHandler(**R5)
+net_connect.enable()
+config_commands = ['interface Tunnel0',
+                   'ip address 155.1.0.5 255.255.255.0',
+                   'ip mut 1400',
+                   'ip nhrp authentication NHRPPASS',
+                   'ip nhrp map multicast dynamic',
+                   'ip nhrp network-id 1',
+                   'ip tcp adjust-mss 1360',
+                   'delay 1000',
+                   'tunnel source Ethernet0/0.100',
+                   'tunnel mode gre multipoint',
+                   'tunnel key 150',
+                   'tunnel protection ipsec profile DMVPN_PROFILE',
+                   'no shutdown']
+output = net_connect.send_config_set(config_commands)
+print(output)
+net_connect.disconnect()
